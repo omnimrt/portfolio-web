d1b69267488d8ff4fc3d63fbcf6ad63b49c09302
Martin Myhalyna <martinmyhalyna@gmail.com>
2024-09-14 12:48:51 +0200

    1234

--- src/formSubmit.js
+++ src/formSubmit.js
@@ -1,10 +1,8 @@
 document.getElementById('contactForm').addEventListener('submit', async e => {
-  e.preventDefault();
+  e.preventDefault(); // Prevents the form from redirecting
 
   const form = document.getElementById('contactForm');
   const formData = new FormData(form);
-
-  console.log('Form submitted'); // Check if the form is being submitted
 
   try {
     const response = await fetch('https://formspree.io/f/mgegvpqe', {
@@ -15,17 +13,14 @@ document.getElementById('contactForm').addEventListener('submit', async e => {
       },
     });
 
-    console.log('Response status:', response.status);
-    console.log('Response data:', await response.json());
-
     if (response.ok) {
-      document.getElementById('formMessage').style.display = 'block';
+      // Show success message and reset the form
+      document.getElementById('formMessage').style.display = 'block'; // Example: success message
       form.reset();
     } else {
       alert('Failed to send message. Please try again.');
     }
   } catch (error) {
-    console.error('Error:', error);
     alert('Oops! There was a problem.');
   }
 });
